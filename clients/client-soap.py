3601398ee6dc76fbc7b5da9c955b47442270e762
Bernard Erick <bernard.programmer@gmail.com>
2023-12-11 18:14:16 -0300

    123

--- clients/client-soap.py
+++ clients/client-soap.py
@@ -7,10 +7,14 @@ soap_url = "http://localhost:3000/?wsdl"
 client = Client(soap_url)
 
 # Chama a função getAllUsers do serviço
-result = client.service.getAllUsers()
+result_users = client.service.getAllUsers()
+result_songs = client.service.getAllSongs()
+result_user_playlists = client.service.getUserPlaylists(user_id)
+result_playlist_songs = client.service.getPlaylistSongs(playlist_id)
+result_song_playlists = client.service.getSongPlaylists(song_id)
 
 # Exibe os resultados
 print("Lista de Usuários:")
-for user in result:
+for user in result_users:
     print(f"ID: {user['id']}, Nome: {user['nome']}, Idade: {user['idade']}")
 
