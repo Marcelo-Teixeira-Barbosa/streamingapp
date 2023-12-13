1fab9b8d388df7758b201f4d549fb4d6b81ed149
Erick Bernardo <bernard.programmer@gmail.com>
2023-12-13 12:25:08 -0300

    123

--- clients/client-soap.py
+++ clients/client-soap.py
@@ -13,13 +13,13 @@ response_songs = client.service.list_all_songs()
 print("Lista de músicas:", response_songs)
 
 # Exemplo de chamada para listar playlists de um usuário específico
-response_playlists = client.service.list_user_playlists(username="User1")
-print("Playlists do usuário User1:", response_playlists)
+response_playlists = client.service.list_user_playlists(id_usuario=2)
+print("Playlists do usuário com o id 2:", response_playlists)
 
 # Exemplo de chamada para listar músicas de uma playlist específica
-response_playlist_songs = client.service.list_playlist_songs(playlist_name="Playlist1")
-print("Músicas da Playlist1:", response_playlist_songs)
+response_playlist_songs = client.service.list_playlist_songs(nome="Playlist D")
+print("Músicas da Playlist D:", response_playlist_songs)
 
 # Exemplo de chamada para listar playlists que contêm uma música específica
-response_song_playlists = client.service.list_song_playlists(song_name="SongA")
-print("Playlists que contêm a música SongA:", response_song_playlists)
+response_song_playlists = client.service.list_song_playlists(song_name="Song A")
+print("Playlists que contêm a música Song A:", response_song_playlists)
--- streamingapp/soap.py
+++ streamingapp/soap.py
@@ -1,32 +1,69 @@
 from spyne import Application, rpc, ServiceBase, Iterable, Unicode
 from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
+import json
 
 class MusicService(ServiceBase):
     @rpc(_returns=Iterable(Unicode))
     def list_all_users(self):
-        # Implementar lógica para listar dados de todos os usuários
-        return ["User1", "User2", "User3"]
+        with open('../db/users.json', 'r') as file:
+            users_data = json.load(file)
+
+        user_names = [user["name"] for user in users_data]
+        return user_names
 
     @rpc(_returns=Iterable(Unicode))
     def list_all_songs(self):
-        # Implementar lógica para listar dados de todas as músicas
-        return ["Song1", "Song2", "Song3"]
+        with open('../db/songs.json', 'r') as file:
+            songs_data = json.load(file)
+
+        songs_data = [song["nome"] for song in songs_data]
+        return songs_data
 
     @rpc(Unicode, _returns=Iterable(Unicode))
-    def list_user_playlists(self, username):
-        # Implementar lógica para listar dados de todas as playlists de um usuário
-        return ["Playlist1", "Playlist2", "Playlist3"]
+    def list_user_playlists(self, id_usuario):
+        with open('../db/playlists.json', 'r') as file:
+            playlists_data = json.load(file)
+        
+        # Filtrar playlists com base no id_usuario
+        user_playlists = [playlist["nome"] for playlist in playlists_data if playlist["id_usuario"] == int(id_usuario)]
+
+        return user_playlists
 
     @rpc(Unicode, _returns=Iterable(Unicode))
-    def list_playlist_songs(self, playlist_name):
-        # Implementar lógica para listar dados de todas as músicas de uma playlist
-        return ["SongA", "SongB", "SongC"]
+    def list_playlist_songs(self, nome):
+        with open('../db/playlists.json', 'r') as file:
+            playlists_data = json.load(file)
+        
+        # Encontrar a playlist com o nome fornecido
+        playlist = next((playlist for playlist in playlists_data if playlist["nome"] == nome), None)
+
+        if playlist:
+            # Retornar a lista de músicas da playlist encontrada
+            return [str(song_id) for song_id in playlist["songs"]]
+        else:
+            return []
 
     @rpc(Unicode, _returns=Iterable(Unicode))
     def list_song_playlists(self, song_name):
-        # Implementar lógica para listar dados de todas as playlists que contêm uma música
-        return ["PlaylistX", "PlaylistY", "PlaylistZ"]
+        # Ler dados do arquivo songs.json
+        with open('../db/songs.json', 'r') as file:
+            songs_data = json.load(file)
+
+        # Encontrar o ID da música com o nome fornecido
+        song_id = next((song["id"] for song in songs_data if song["nome"] == song_name), None)
+
+        if song_id is not None:
+            # Ler dados do arquivo playlists.json
+            with open('../db/playlists.json', 'r') as file:
+                playlists_data = json.load(file)
+
+            # Encontrar playlists que contenham o ID da música na lista de músicas
+            playlists_containing_song = [playlist["nome"] for playlist in playlists_data if song_id in playlist["songs"]]
+
+            return playlists_containing_song
+        else:
+            return []
 
 application = Application([MusicService],
                           tns='music_service',
